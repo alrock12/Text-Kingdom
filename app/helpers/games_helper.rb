514199aeb8344a35e47bb1e5d60bd9c52798ce17
alrock12 <amy.rockwood@yale.edu>
2014-07-08 10:15:39 -0400

    Blocked access and redirect to sign-in

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,16 +1,16 @@
 module GamesHelper
-	def rating (game)
-      sum = 0
-      count = 0
-      game.experiences.each do |experience|
-        sum += experience.rating
-        count++
-      end
+# 	def rating (game)
+#       sum = 0
+#       count = 0
+#       game.experiences.each do |experience|
+#         sum += experience.rating
+#         count++
+#       end
 
-      if count == 0
-      	return 0
-      else
-      	return sum.to_f / count
-      end
-    end
+#       if count == 0
+#       	return 0
+#       else
+#       	return sum.to_f / count
+#       end
+#     end
 end
