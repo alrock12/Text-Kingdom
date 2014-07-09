13d06d2863866923908ba16d9933d3969af8cb5c
alrock12 <amy.rockwood@yale.edu>
2014-07-09 15:47:09 -0400

    added ratings to user page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,18 @@ module ApplicationHelper
       n != 0 ? avg = (sum/n).round(1) : avg = 0.0
     end
 
+  def rating (game)
+      game.experiences.average(:rating).to_f.round(1)
+  end
+
+  def plays(game)
+    game.experiences.count
+  end
+
+  def game_total_plays (game)
+      game.experiences.count
+  end
+
 
 
 
